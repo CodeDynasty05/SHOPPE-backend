e3a11cef289aa024e0900ebc589706a810c2e07e
CodeDynasty05 <qenizadenazim@gmail.com>
2025-02-13 17:08:57 +0400

    Initial commit

---
+++ src/test/java/com/matrix/SHOPPE/ShoppeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrix.SHOPPE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
