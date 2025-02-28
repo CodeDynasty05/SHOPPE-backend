40893771866e87309df2ee4722a4cac7d9244536
CodeDynasty05 <qenizadenazim@gmail.com>
2025-02-28 22:29:36 +0400

    Database connection using JPA

--- src/main/java/com/matrix/SHOPPE/ShoppeApplication.java
+++ src/main/java/com/matrix/SHOPPE/ShoppeApplication.java
@@ -23,7 +23,7 @@ public class ShoppeApplication implements CommandLineRunner {
 
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) {
 		for (User user : userRepository.findAll()) {
 			log.info("Category: {}", user);
 		}
